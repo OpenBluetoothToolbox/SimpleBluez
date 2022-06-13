361a5555f67c74992ace2332db4ca1c19f6a664b
Kevin Dewald <kevin.dewald@ieee.org>
2022-06-12 19:37:51 -0700

    Updated docs for release

--- cmake/find/Findsimpledbus.cmake
+++ cmake/find/Findsimpledbus.cmake
@@ -7,7 +7,7 @@ if (SIMPLEDBUS_VENDORIZE)
         set(SIMPLEDBUS_GIT_REPOSITORY "https://github.com/OpenBluetoothToolbox/SimpleDBus.git")
     endif()
     if(NOT SIMPLEDBUS_GIT_TAG)
-        set(SIMPLEDBUS_GIT_TAG "debug/release") # TODO: Switch to the latest SimpleDBus release once done.
+        set(SIMPLEDBUS_GIT_TAG "v2.2.0")
     endif()
 
     if(NOT SIMPLEDBUS_LOCAL_PATH)
